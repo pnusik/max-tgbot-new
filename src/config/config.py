30724488bf01cbaef429fd46b900d90f0eacf8d6
Andrey <pnusik2010@gmail.com>
2026-09-06 12:36:45 +0700

    test

--- src/config/config.py
+++ src/config/config.py
@@ -1,3 +1,8 @@
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+
 class Global_config:
     def __init__(self):
         self.path_to_download_folder = "/home/andrey/Desktop/progects/Telegrammaxbot/downloads"
@@ -5,13 +10,14 @@ class Global_config:
 class TG_cofnig(Global_config):
     def __init__(self):
         super().__init__()
-        self.BOT_TOKEN="8111294889:AAFNZaTTlsLHlCF1kJfIb9naVEBgXELY6T4"
-        self.Chat_id=-1003994286063
+        self.BOT_TOKEN = getenv("TG_BOT_TOKEN")
+        self.Chat_id = int(getenv("TG_CHAT_ID"))
         self.Thread_id=2
         self.react_to_sucsess = "👍"
 
 class MAX_config(Global_config):
     def __init__(self):
         super().__init__()
-        self.BOT_TOKEN="f9LHodD0cOJRre0JlJmw0Op5lwZmV-6WkE6EFdv99e2FAxJ_QO98_S3RLO7X3zjumSwNLNQwaC6iR-0Kvkw8"
-        self.Chat_id=-78472421107172
+        self.BOT_TOKEN = getenv("MAX_BOT_TOKEN")
+        self.Chat_id = int(getenv("MAX_CHAT_ID"))
+
